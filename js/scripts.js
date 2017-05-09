b4f99b49245641d4c01f526e6e234254d6cce7e2
Mownisha <reach.mownisha@gmail.com>
2017-05-08 17:19:00 -0700

    added javascript and jquery to the file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var translate = function(word) {
+    var array = word.split('');
+    var vowels = ['a','e','i','o','u'];
+    var newWord = '';
+    for(var i = 0; i < vowels.length-1; i++) {
+        for(var y = 0; y < word.length-1; y++) {
+            if(word[y] === vowels[i]) {
+                for(var x = y; x < word.length; x++){
+                    newWord = newWord + word[x];
+                }
+                for(var n = 0; n < y; n++){
+                    newWord = newWord + word[n];
+                }
+                return newWord + "ay";
+            }
+        }
+    }
+};
+
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    var str =$("#input").val();
+    var result = translate(str);
+    $(".result").text(result);
+    $("#result-div").show();
+  });
+});
